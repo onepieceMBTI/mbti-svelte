73d16687a2a61c3b14e92fdbc6ca01317f97f1f7
eonwy <yeawon6566@gmail.com>
2024-06-13 22:02:48 +0900

    made structure & upload png(only for mainpage)

---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Counter from '../Counter.svelte';
+    import dialogBox from '$lib/images/dialogBox.png'
+    import mainPageIllust from '$lib/images/mainPage.png'
+</script>
+
+<p>원피스 캐릭터 중 나와 mbti가 비슷한 캐릭터는?</p>
+<p>시작하기</p>
+
+<picture>
+    <img src={dialogBox} alt="dialogBox" />
+    <img src={mainPageIllust} alt="illust" />
+</picture>
