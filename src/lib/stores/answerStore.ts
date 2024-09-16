947382eff2b090ca10280cc4ea0957a8ffca6546
eonwy <yeawon6566@gmail.com>
2024-09-17 00:50:58 +0900

    쓸모없는 코드 정리

--- src/lib/stores/answerStore.ts
+++ src/lib/stores/answerStore.ts
@@ -1,14 +1,11 @@
-// src/lib/stores/answerStore.ts
 import { writable } from 'svelte/store';
 
-const savedAnswers =
-	typeof window !== 'undefined' && localStorage.getItem('selectedAnswers')
-		? JSON.parse(localStorage.getItem('selectedAnswers')!)
-		: [];
+const isBrowser = typeof window !== 'undefined';
+const savedAnswers = isBrowser ? JSON.parse(localStorage.getItem('selectedAnswers') || '[]') : [];
 
 export const selectedAnswers = writable<number[]>(savedAnswers);
 
-if (typeof window !== 'undefined') {
+if (isBrowser) {
 	selectedAnswers.subscribe((value) => {
 		localStorage.setItem('selectedAnswers', JSON.stringify(value));
 	});
